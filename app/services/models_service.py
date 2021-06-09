81664337de51c430bfd0da024807d0303cbc5bb3
Carolina Oliveira <carolinasao6@gmail.com>
2021-06-08 21:02:32 -0300

    Feat: finalizadas funcoes models_service

--- app/services/models_service.py
+++ app/services/models_service.py
@@ -4,6 +4,7 @@ from .conn_cur_service import conn_cur, end_conn_cur
 class AnimeTable:
 
     table_header = ['id', 'anime', 'released_date', 'seasons']
+    required_fields = ['anime', 'released_date', 'seasons']
 
     @staticmethod
     def create_table() -> None:
@@ -23,4 +24,72 @@ class AnimeTable:
 
         end_conn_cur(conn, cur)
 
-        
+    def check_fields(self, data: dict):
+        recived_keys = data.keys()
+
+        return [required for required in self.required_fields if required not in recived_keys]
+
+    def create_anime(self, data: dict):
+        conn, cur = conn_cur()
+        self.create_table()
+
+        data['anime'] = data['anime'].title()
+
+        cur.execute(
+            """
+                INSERT INTO animes
+                    (anime, released_date, seasons)
+                VALUES
+                    (%(anime)s, %(released_date)s, %(seasons)s)
+                RETURNING *
+            """,
+            data,
+        )
+        query = cur.fetchone()
+
+        end_conn_cur(conn, cur)
+
+        result = dict(zip(self.table_header, query))
+
+        result["released_date"] = result["released_date"].strftime("%d/%m/%Y")
+
+        return result
+    
+    def return_data(self):
+        conn, cur = conn_cur()
+
+        if AnimeTable.create_table(self) == False:
+            return []
+
+        cur.execute("SELECT * FROM animes")
+
+        query = cur.fetchall()
+
+        end_conn_cur(conn, cur)
+
+        result = [dict(zip(self.table_header, show_table)) for show_table in query]
+
+        for data in result:
+            data["released_date"] = data["released_date"].strftime("%d/%m/%Y")
+
+        return result
+
+    def select_id(self, serie_id):
+        conn, cur = conn_cur()
+
+        try:
+            
+            cur.execute("SELECT * FROM aimes WHERE id = %(serie_id)s", {"serie_id": serie_id})
+
+            query = cur.fetchone()
+
+            end_conn_cur(conn, cur)
+
+            result = dict(zip(self.table_header, query))
+    
+            return {"data": result}
+
+        except:
+
+            AnimeTable.create_table(self)
+            return {"error": "Not Found"}, 404
